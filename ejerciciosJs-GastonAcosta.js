4c5693f2ccaeff98a857641a0bf051ce16323a55
gasteac <gastoneacosta@outlook.com>
2022-12-02 11:41:39 -0300

    re-hecho porque si

--- ejerciciosJs-GastonAcosta.js
+++ ejerciciosJs-GastonAcosta.js
@@ -1,47 +1,51 @@
-// Ejercicio 1 
-// This is an in-line comment.
-/* This is a
-multi-line comment */
+// ########## Ejercicio 1 ##########
 
-//Ejercicio 2
+// Soy un comentario de linea
+
+/* Soy
+un
+comentario
+multilinea */
+
+// ########## Ejercicio 2 ##########
+
 var myName;
 
-//Ejercicio 3
+// ########## Ejercicio 3 ##########
+
 // Setup
 var a;
 a = 7;
 
-// Only change code below this line
-
-//Ejercicio 4
+// ########## Ejercicio 4 ##########
 // Setup
 var a;
 a = 7;
 var b;
-b = 7;
+
+
 b = a;
 
-// Only change code below this line
-
-//Ejercicio 5
+// ########## Ejercicio 5 ##########
 var a = 9;
 
-//Ejercicio 6
+// ########## Ejercicio 5 ##########
 var myFirstName = "Gaston";
-var myLastName = "Acosta";
+var myLastName = "Acosta"
 
-//Ejercicio 7
-// Only change code below this line
-var a = 5;
-var b = 10;
-var c ="I am a";
+// ########## Ejercicio 7 ##########
+var a;
+var b;
+var c;
 // Only change code above this line
-
+a = 5
+b = 10
+c = "I am a"
 a = a + 1;
 b = b + 5;
 c = c + " String!";
 
-//Ejercicio 8
+// ########## Ejercicio 8 ##########
 // Variable declarations
 var studlyCapVar;
 var properCamelCase;
@@ -52,124 +56,114 @@ studlyCapVar = 10;
 properCamelCase = "A String";
 titleCaseOver = 9000;
 
-//Ejercicio 9
+// ########## Ejercicio 9 ##########
 let catName = "Oliver";
 let catSound = "Meow!";
 
-//Ejercicio 10
-const FCC = "freeCodeCamp"; // Change this line
-let fact = "is cool!"; // Change this line
+// ########## Ejercicio 10 ##########
+const FCC = "freeCodeCamp"; 
+let fact = "is cool!"; 
 fact = "is awesome!";
-console.log(FCC, fact); // Change this line
+console.log(FCC, fact); 
 
-//Ejercicio 11
+// ########## Ejercicio 11 ##########
 const sum = 10 + 10;
 
-//Ejercicio 12
+// ########## Ejercicio 12 ##########
 const difference = 45 - 33;
 
-//Ejercicio 13
+// ########## Ejercicio 13 ##########
 const product = 8 * 10;
 
-//Ejercicio 14
+// ########## Ejercicio 14 ##########
 const quotient = 66 / 33;
 
-//Ejercicio 15
+// ########## Ejercicio 15 ##########
 let myVar = 87;
 
-// Only change code below this line
 myVar++;
 
-//Ejercicio 16
+// ########## Ejercicio 16 ##########
 let myVar = 11;
 
-// Only change code below this line
 myVar--;
 
-//Ejercicio 17
+// ########## Ejercicio 17 ##########
 const ourDecimal = 5.7;
 
-// Only change code below this line
-let myDecimal = 5.7;
+let myDecimal = 4.33
 
-//Ejercicio 18
-const product = 2.5 * 2.0;
+// ########## Ejercicio 18 ##########
+const product = 2.0 * 2.5;
 
-//Ejercicio 19
-const quotient = 4.4 / 2.0; // Change this line
+// ########## Ejercicio 19 ##########
+const quotient = 4.4 / 2.0; 
 
-//Ejercicio 20
-let remainder = 0;
-remainder = 11 % 3;
+// ########## Ejercicio 20 ##########
+const remainder = 11 % 3;
 
-//Ejercicio 21
+// ########## Ejercicio 21 ##########
 let a = 3;
 let b = 17;
 let c = 12;
 
-// Only change code below this line
-a += 12;
-b += 9;
-c += 7;
+a+= + 12;
+b+= 9;
+c+= 7;
 
-//Ejercicio 22
+// ########## Ejercicio 22 ##########
 let a = 11;
 let b = 9;
 let c = 3;
 
-// Only change code below this line
 a -= 6;
 b -= 15;
 c -= 1;
 
-//Ejercicio 23
+// ########## Ejercicio 23 ##########
 let a = 5;
 let b = 12;
 let c = 4.6;
 
-// Only change code below this line
 a *= 5;
 b *= 3;
 c *= 10;
 
-//Ejercicio 24
+// ########## Ejercicio 24 ##########
 let a = 48;
 let b = 108;
 let c = 33;
 
-// Only change code below this line
 a /= 12;
 b /= 4;
 c /= 11;
 
-//Ejercicio 25
-const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+// ########## Ejercicio 25 ##########
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; 
 
-//Ejercicio 26
+// ########## Ejercicio 26 ##########
 const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
 
-//Ejercicio 27
-const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";// Change this line
+// ########## Ejercicio 27 ##########
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
 
-//Ejercicio 28
-const myStr = "This is the start. " + "This is the end."; // Change this line
+// ########## Ejercicio 28 ##########
+const myStr = "This is the start. " + "This is the end."; 
 
-//Ejercicio 29
+// ########## Ejercicio 29 ##########
 let myStr ="This is the first sentence. "
 myStr += "This is the second sentence.";
 
-//Ejercicio 30
-// Only change code below this line
+// ########## Ejercicio 30 ##########
 const myName = "Gaston";
-const myStr = "My name is" + myName + "and I am well!";
+const myStr = "My name is" +myName + " and I am well!";
 
-//Ejercicio 31
-// Change code below this line
-const someAdjective = "Wonderfull";
+// ########## Ejercicio 31 ##########
+const someAdjective = "banana";
 let myStr = "Learning to code is ";
-myStr += someAdjective;
+myStr+= someAdjective;
 
-//Ejercicio 32
+// ########## Ejercicio 32 ##########
 // Setup
 let lastNameLength = 0;
 const lastName = "Lovelace";
@@ -177,57 +171,55 @@ const lastName = "Lovelace";
 // Only change code below this line
 lastNameLength = lastName.length;
 
-//Ejercicio 33
+// ########## Ejercicio 33 ##########
 // Setup
 let firstLetterOfLastName = "";
 const lastName = "Lovelace";
 
 // Only change code below this line
-firstLetterOfLastName = lastName[0]; // Change this line
+firstLetterOfLastName = lastName[0]; 
 
-//Ejercicio 34
+// ########## Ejercicio 34 ##########
 // Setup
 let myStr = "Jello World";
 
 // Only change code below this line
 myStr = "Hello World"; // Change this line
-// Only change code above this line
 
-//Ejercicio 35
+// ########## Ejercicio 35 ##########
 // Setup
 const lastName = "Lovelace";
 
 // Only change code below this line
-const thirdLetterOfLastName = lastName[2]; // Change this line
+const thirdLetterOfLastName = lastName[2]; 
 
-//Ejercicio 36
+// ########## Ejercicio 36 ##########
 // Setup
 const lastName = "Lovelace";
 
 // Only change code below this line
-const lastLetterOfLastName = lastName [lastName.length-1]; // Change this line
+const lastLetterOfLastName = lastName[lastName.length -1];
 
-//Ejercicio 37
+// ########## Ejercicio 37 ##########
 // Setup
 const lastName = "Lovelace";
 
 // Only change code below this line
-const secondToLastLetterOfLastName = lastName[lastName.length -2]; // Change this line
+const secondToLastLetterOfLastName = lastName[lastName.length -2];
 
-//Ejercicio 38
+// ########## Ejercicio 38 ##########
 const myNoun = "dog";
 const myAdjective = "big";
 const myVerb = "ran";
 const myAdverb = "quickly";
 
 // Only change code below this line
-const wordBlanks = myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + "." ; // Change this line
-// Only change code above this line
+const wordBlanks = "The " + myNoun + " was so " + myAdjective + " he " + myVerb + " so " + myAdverb;
 
-//Ejercicio 39
+// ########## Ejercicio 39 ##########
 // Only change code below this line
-const myArray = ["peanut butter", 2];
+const myArray = ['LoboNegro', 33];
 
-//Ejercicio 40
+// ########## Ejercicio 40 ##########
 // Only change code below this line
-const myArray = [["Bulls", 23], ["White Sox", 45]];
+const myArray = [['Lobito', 21], ['Perrito', 28]];
