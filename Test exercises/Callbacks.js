b1aae2a6fbf553ee06d360db8007c88d9e790a8e
gasteac <gastoneacosta@outlook.com>
2022-12-06 12:32:15 -0300

    Callbacks

---
+++ Test exercises/Callbacks.js
@@ -0,0 +1,24 @@
+function callback(resultado){
+    console.log('callback', resultado)
+}
+
+function operacion(a,b,cb){
+    let result = 0;
+    //Callback async 
+    setTimeout(()=> {
+        result = a + b;
+        cb(result) //cb es mi funcion callbacl
+    }, 1000);
+}
+
+operacion(2, 3, callback)
+
+
+const arreglo =[1,2,3];
+
+function filter(cb){
+    // Callback sync
+    arreglo.forEach(element => cb(element))
+}
+
+filter((element) => console.log(element))
--- Test exercises/Loops.js
+++ Test exercises/Loops.js
@@ -1,1 +1,28 @@
-a
+// for (let i = 1 ; i <= 10000 ; i +=1){
+//     console.log('i es =', i);
+// }
+
+const persona = {
+    name:'gaston',
+    lastname:'acosta',
+    edad:27
+}
+
+for (let property in persona){
+    console.log('propiedad:', property)
+    console.log('valor propiedad:', persona[property])
+}
+
+
+while (persona.edad > 2){
+    console.log(persona.edad)
+    persona.edad -= 1
+}
+
+i = 1000
+do{
+    console.log('hola')
+    i-= 1
+} while (i > 0);
+
+
