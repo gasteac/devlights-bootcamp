5948a4f02cc3d5f8fb9ed57590887e71d8caf080
gasteac <gastoneacosta@outlook.com>
2022-12-07 21:07:37 -0300

    arrays started

---
+++ Ejercicios entrega/Javascript Intermedio 29-11/basicDataStructures.js
@@ -0,0 +1,11 @@
+// 1.Use an Array to Store a Collection of Data
+
+let yourArray = ['hola', 5, [1,2,3], {nombre:'Maria'}, true]; 
+
+// 2.Access an Array's Contents Using Bracket Notation
+
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[1] = 'k'
+// Only change code above this line
+console.log(myArray);
