4eef625884ccba3d550d2003279a3fe8ef7fecdf
gasteac <gastoneacosta@outlook.com>
2022-12-27 01:41:21 -0300

    express

---
+++ ejerciciosClases/clase8/express-api/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const server = express();
+
+server.get("/", (req, res) => {
+    res.send("Hola Mundo");
+})
+
+server.get("/contact", (req, res) => {
+    res.send("Contacto");
+})
+
+server.get("/about", (req, res) => {
+    res.send("Acerca de");
+})
+
+server.get("/:param1/:param2", (req, res) => {
+    res.send(`Mis parametros son: ${JSON.stringify(req.params)}. Mis query params son: ${JSON.stringify(req.query)}`);
+})
+
+server.listen(3000);
